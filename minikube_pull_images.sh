8c8712d97a3f63794c46a490cf85a922310a6664
cheneydc <cheneydc@gmail.com>
2018-04-19 17:33:48 +0800

    Update tiller to 2.8.1 version

--- minikube_pull_images.sh
+++ minikube_pull_images.sh
@@ -54,8 +54,8 @@ docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/defaultbackend:1.4
 docker tag registry.cn-hangzhou.aliyuncs.com/dck8s/defaultbackend:1.4 k8s.gcr.io/defaultbackend:1.4
 
 # tiller
-docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/tiller:v2.8.2
-docker tag registry.cn-hangzhou.aliyuncs.com/dck8s/tiller:v2.8.2 gcr.io/kubernetes-helm/tiller:v2.8.2
+docker pull registry.cn-hangzhou.aliyuncs.com/dck8s/tiller:v2.8.1
+docker tag registry.cn-hangzhou.aliyuncs.com/dck8s/tiller:v2.8.1 gcr.io/kubernetes-helm/tiller:v2.8.1
 
 # coredns
 docker pull registry.cn-qingdao.aliyuncs.com/dck8s/coredns:1.0.6
